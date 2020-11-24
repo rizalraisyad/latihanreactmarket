2ceb163261c29db1e8c919ce3dfb7d38fd630cdb
rizalraisyad <muharamrizal2@gmail.com>
2020-11-24 18:48:06 +0700

    repo first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {Switch, Router} from 'react-router-dom'
+import React, {Component} from 'react'
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import Navbar from './components/Navbar'
+import ProductList from './components/ProductList'
+import Details from './components/Details'
+import Default from './components/Default'
+import Cart from './components/Cart'
+import { Route } from 'react-router-dom/cjs/react-router-dom.min'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={ProductList}/>
+        <Route path="/details" component={Details}/>
+        <Route path="/cart" component={Cart}/>
+        <Route component={Default}/>
+
+      </Switch>
+    </React.Fragment>
   );
 }
 
